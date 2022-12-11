4f166b069996f1b2cb570d6df775d7097986b555
0xjimmy <contact@decentralised.tech>
2022-12-11 20:34:39 +1300

    Add deploy and upgrade scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  etherscan: {
+    apiKey: {
+      bscTestnet: process.env.BSC_TESTNET_ETHERSCAN_API_KEY ?? "",
+      polygonMumbai: process.env.POLYGON_MUMBAI_ETHERSCAN_API_KEY ?? "",
+      ftmTestnet: process.env.FTM_TESTNET_ETHERSCAN_API_KEY ?? "",
+    },
+  },
 };
 
 export default config;
