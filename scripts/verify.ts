6f7a1bf3d97cd9fea30492bd40b08d3ff5a183c3
kazii <kazdan@seznam.cz>
2023-01-02 16:59:32 +0100

    Added ERCReceiver, working one

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -3,18 +3,18 @@ import { run, network } from "hardhat";
 const verify = async () => {
   if (network.name !== "hardhat") {
     console.log("Verifying contract source on blockchain scan ...");    await run("verify:verify", {
-      address: "0xb4a14B084A3F7B7A2f5039CD086641164790B9F6",
+      address: "0xB6A1a1b92f13676D5575B56A82Fd610747477686",
       constructorArguments: [],
     });
   console.log("Verified Contract 3/3: RewardFacet");
     await run("verify:verify", {
-      address: "0xEf6d8731D1E51A30d22B928d04112fdB9bEb44e9",
+      address: "0x7eb38C49966A348060f62B942C1e4D87F4024AB7",
       constructorArguments: [],
     });
     console.log("Verified Contract 1/3: MasterFacet");
 
     await run("verify:verify", {
-        address: "0x39d273F226cCE96a354A9Dc1181922fd4C98bC99",
+        address: "0xa4fa0C3224f60730f20F972eab781d7CCbe868cE",
         constructorArguments: [],
       });
     console.log("Verified Contract 2/3: FundFacet");
